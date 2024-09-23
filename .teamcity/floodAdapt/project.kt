4671f757a6dd7ea12f4c893ab56a087b5cefc44d
Jaap <33715902+Jaapel@users.noreply.github.com>
2024-09-23 09:29:48 +0200

    add project to settings.kt

--- .teamcity/floodAdapt/project.kt
+++ .teamcity/floodAdapt/project.kt
@@ -8,4 +8,12 @@ object project : Project({
     vcsRoot(FloodAdaptGUI)
     vcsRoot(FloodAdaptCICD)
     vcsRoot(FloodAdaptDataBase)
+
+    description = "A project to manage CI/CD structures in FloodAdapt"
+
+    params {
+        password("FloodAdapt-GUI-ssh-passphrase", "credentialsJSON:9ef5e4f0-0c04-4b45-84ef-2ea44d0ea990")
+        password("FloodAdaptCICD-ssh-passphrase", "credentialsJSON:9ef5e4f0-0c04-4b45-84ef-2ea44d0ea990")
+        password("FloodAdapt-ssh-passphrase", "credentialsJSON:9ef5e4f0-0c04-4b45-84ef-2ea44d0ea990")
+    }
 })
